fd43ab2b51a7e5e29799bbebeea9395aa3ad06ba
Krishendree Kistensamy <krishendree@gmail.com>
2026-05-25 12:08:30 +0200

    Add files via upload

---
+++ Conditional logic/conditionals.js
@@ -0,0 +1,26 @@
+let Score = 85;  
+// Let because the value can be changed
+
+const PassingGrade = 60;
+// Const because the value cannot be changed
+
+
+if (Score >= PassingGrade) {
+    console.log("You passed, well done!");
+}
+// else if is used to check if a condition is true
+
+else {
+    console.log("Failed");
+}
+// else is used to check if a condition doesn't meet the if statement
+
+if (Score === 100) {
+    console.log("Congratulations! You got a perfect score!");
+} 
+// if is used to check if a condition is true
+// using === checks if the value is equal to the value
+
+
+// krishendree - finished stating let and const for the score and passingGrade variables
+// Double check the code and help with the if and else statement, please Owam
