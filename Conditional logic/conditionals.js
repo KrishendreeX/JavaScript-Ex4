f0863eef60294790c1dc15e1e5c06ca0b82db625
Krishendree Kistensamy <krishendree@gmail.com>
2026-05-25 13:25:40 +0200

    Update conditionals.js

--- Conditional logic/conditionals.js
+++ Conditional logic/conditionals.js
@@ -6,18 +6,25 @@ const PassingGrade = 60;
 
 
 if (Score >= PassingGrade) 
-    console.log("You passed!");
-// else if is used to check if a condition is true
+    console.log("Passed!, well done!");
+// if is used to check if a condition is true 
 
 else 
     console.log("Failed");
-// else is used to check if a condition doesn't meet the if statement
+// else is used to check if a condition doesn't meet the if statements
 
 if (Score === 100) 
-    console.log("Congratulations , you have a perfect score!");
+    console.log("Congratulations!, you got a perfect score!");
 // if is used to check if a condition is true
 // using === checks if the value is equal to the value
 
 
+
 // krishendree - finished stating let and const for the score and passingGrade variables
-// Owam did the if and else statement
+// Please double check the code and help with the if and else statements, Owam
+
+// Owam - Checked the if and else statements, found an error and corrected it!
+// All statements works perfectly
+
+// Krishendree - Double checked everything, edited the code to be more readable
+// Added in comments and edited the text for passing and failing messages
