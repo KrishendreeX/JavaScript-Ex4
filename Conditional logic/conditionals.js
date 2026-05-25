3bc5cda453ae669fa46516ddf2e1a31878da424f
OwamGobs <owamgobingca@gmail.com>
2026-05-25 03:36:59 -0700

    Simplify if-else statements in conditionals.js
    Refactor conditional statements for cleaner syntax and improved readability.

--- Conditional logic/conditionals.js
+++ Conditional logic/conditionals.js
@@ -5,22 +5,19 @@ const PassingGrade = 60;
 // Const because the value cannot be changed
 
 
-if (Score >= PassingGrade) {
+if (Score >= PassingGrade) 
     console.log("You passed!");
-}
 // else if is used to check if a condition is true
 
-else {
+else 
     console.log("Failed");
-}
 // else is used to check if a condition doesn't meet the if statement
 
-if (Score === 100) {
-    console.log("Congratulations!");
-} 
+if (Score === 100) 
+    console.log("Congratulations , you have a perfect score!");
 // if is used to check if a condition is true
 // using === checks if the value is equal to the value
 
 
 // krishendree - finished stating let and const for the score and passingGrade variables
-// Double check the code and help with the if and else statement, please Owam
+// Owam did the if and else statement
